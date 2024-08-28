85d1902d2ddba327477be191b75634d4fa64abb7
Jeferson <jefeleal363@gmail.com>
2024-08-28 19:06:59 -0300

    feat: ex14

---
+++ aula14/index.js
@@ -0,0 +1,16 @@
+//let num1 = 1500; //number
+//let num2 = 2.5;//number
+
+//concatenar
+//console.log(num1.toString() + num2.toString())
+
+
+//let num1 = 1500; //number
+//console.log(num1.toString(2))
+
+//let num1 = 10.6756476545747;
+//console.log(num1.toFixed(2))
+
+//let num1 = 10
+
+//console.log(Number.isInteger(num1))
---
+++ aula14/tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+let num1 = 10.6756476545747;
+console.log(num1.toFixed(2))
